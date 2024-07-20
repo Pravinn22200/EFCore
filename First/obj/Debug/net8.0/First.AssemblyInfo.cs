5c237b2f087680ba3744f66dc4b015b1186454e5
Pravinn22200 <Pravinn22200@gmail.com>
2024-07-20 13:14:43 +0530

    file deleted commit

--- First/obj/Debug/net8.0/First.AssemblyInfo.cs
+++ First/obj/Debug/net8.0/First.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47238bd35822175d6b27fc78bccfb2993e881d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
